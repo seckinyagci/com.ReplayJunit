33cdef7f216b3d610582f0931c8c5913161932ba
seckinyagci <seckininadresi@gmail.com>
2022-10-07 22:50:08 +0200

    iframe

---
+++ src/test/java/Tag10_Iframe/C01_Iframe.java
@@ -0,0 +1,52 @@
+package Tag10_Iframe;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_Iframe {
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @AfterClass
+    public static void tearDown(){
+
+        driver.close();
+    }
+    //● Bir class olusturun: IframeTest
+    //● https://the-internet.herokuapp.com/iframe adresine gidin.
+    //● Bir metod olusturun: iframeTest
+
+
+    //○ TextBox’in altinda bulunan “Elemental Selenium” linkini textinin gorunur oldugunu
+    //dogrulayin ve konsolda yazdirin
+    @Test
+    public void IframeTest010(){
+        //○ “An IFrame containing….” textinin erisilebilir oldugunu test edin ve konsolda yazdirin.
+        driver.get("https://the-internet.herokuapp.com/iframe");
+        WebElement baslikElementi=driver.findElement(By.xpath("//h3"));
+        Assert.assertTrue(baslikElementi.isEnabled());}
+        @Test
+        public void IframeTest02(){
+        //○ Text Box’a “Merhaba Dunya!” yazin.
+            WebElement textKutusu=driver.findElement(By.xpath("//body[@id="tinymce"]"));
+
+
+
+
+    }
+}
