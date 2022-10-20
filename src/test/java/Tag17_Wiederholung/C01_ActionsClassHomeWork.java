6b4bb960060163cae40e0cecd6b380d19bd01032
seckinyagci <seckininadresi@gmail.com>
2022-10-21 01:18:37 +0200

    select

--- src/test/java/Tag17_Wiederholung/C01_ActionsClassHomeWork.java
+++ src/test/java/Tag17_Wiederholung/C01_ActionsClassHomeWork.java
@@ -22,12 +22,11 @@ public class C01_ActionsClassHomeWork extends TestBaseC {
     //2. "Hover over Me First" kutusunun ustune gelin
         WebElement howerOver= driver.findElement(By.xpath("//button[text()=\"Hover Over Me First!\"]"));
     //3. "Link 1" e tiklayin
+        WebElement link1= driver.findElement(By.xpath("//*[@id=\"div-hover\"]/div[1]/div/a"));
         Actions actions=new Actions(driver);
-        actions.moveToElement(howerOver).perform();
-        WebElement ddm= driver.findElement(By.xpath("//div[@class=\"dropdown hover\"]"));
-        Thread.sleep(5000);
-        Select select=new Select(ddm);
-        select.selectByIndex(1);
+        actions.moveToElement(howerOver).click(link1).
+                perform();
+
     //4. Popup mesajini yazdirin
         String message=driver.switchTo().alert().getText();
         System.out.println("pop up mesaji : "+message);
@@ -35,6 +34,13 @@ public class C01_ActionsClassHomeWork extends TestBaseC {
     //5. Popup'i tamam diyerek kapatin
 
     //6. "Click and hold" kutusuna basili tutun
+        WebElement clickAnHold=driver.findElement(By.id("click-box"));
+        actions.clickAndHold(clickAnHold).perform();
     //7. "Click and hold" kutusunda cikan yaziyi yazdirin
+        String clikcAndHoldText=clickAnHold.getText();
+        System.out.println(clikcAndHoldText);
     //8. "Double click me" butonunu cift tiklayin
+        WebElement doubleClick= driver.findElement(By.id("double-click"));
+        actions.doubleClick(doubleClick).perform();
+
 }}
