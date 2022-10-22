f7b1d4505ddaabcda1d805b59b62761c7c89d418
seckinyagci <seckininadresi@gmail.com>
2022-10-22 11:22:15 +0200

    iframeUebung

---
+++ src/test/java/Tag10_Iframe/C02_Ubungen.java
@@ -0,0 +1,41 @@
+package Tag10_Iframe;
+
+import Tag11_WindowHandle_TestBase.utilities.TestBaseC;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class C02_Ubungen extends TestBaseC {
+
+    @Test
+    public void test01() throws InterruptedException {
+        //1. “http://webdriveruniversity.com/IFrame/index.html” sayfasina gidin
+
+        driver.get("http://webdriveruniversity.com/IFrame/index.html");
+        //2. “Our Products” butonuna basin
+        WebElement iframe= driver.findElement(By.id("frame"));
+        driver.switchTo().frame(iframe);
+        driver.findElement(By.xpath("//*[text()='Our Products']")).click();
+        //3. “Cameras product”i tiklayin
+
+        driver.findElement(By.xpath("//*[text()='Cameras']")).click();
+
+
+        //4. Popup mesajini yazdirin
+
+        String popUp=driver.findElement(By.xpath("//*[@id=\"myModal\"]/div/div/div[2]/p")).getText();
+        System.out.println("popup :"+popUp);
+        //5. “close” butonuna basin
+        driver.findElement(By.xpath("//button[text()='Close']")).click();
+        //6. "WebdriverUniversity.com (IFrame)" linkini tiklayin
+        driver.switchTo().parentFrame();
+        driver.findElement(By.xpath("//*[text()='WebdriverUniversity.com (IFrame)']")).click();
+
+        //7. "http://webdriveruniversity.com/index.html" adresine gittigini test edin
+        String expectedUrl="http://webdriveruniversity.com/index.html";
+        String actuelUrl=driver.getCurrentUrl();
+        Assert.assertEquals(expectedUrl,actuelUrl);
+        System.out.println(actuelUrl);
+    }
+}
